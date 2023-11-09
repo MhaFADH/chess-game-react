642920fd264249cd624dda3d0210a092485993f0
MhaFADH <fadhlaoui.mohameda@gmail.com>
2023-11-09 18:35:35 +0100

    Starting project

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+const App = () => (
+  <div className="bg-green-400 grid grid-cols-2">
+    <button>zdsqsq</button>
+    <button>zdsqsq</button>
+    <button>zdsqsq</button>
+    <button>zdsqsq</button>
+    <button>zdsqsq</button>
+  </div>
+)
+
+export default App
