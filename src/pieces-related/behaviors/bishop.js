61775e409bd828df50b11c55dbc72d20c12ce546
MhaFADH <fadhlaoui.mohameda@gmail.com>
2023-11-25 23:56:13 +0100

    bishop complexity under 10, pawn left, maybe undo and then its done

--- src/pieces-related/behaviors/bishop.js
+++ src/pieces-related/behaviors/bishop.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-console */
-
-const diagMove = (payload) => {
+const checkLeft = (payload) => {
   const { source, target, board, direction, threshold } = payload
 
   for (
@@ -22,45 +19,43 @@ const diagMove = (payload) => {
 
   return false
 }
+const checkRight = (payload) => {
+  const { source, target, board, direction, threshold } = payload
+
+  for (
+    let i = direction;
+    source.y + i !== threshold && source.x + i <= 7;
+    i += direction
+  ) {
+    const concerned = board?.[source.y + i]?.[Math.abs(source.x + i)]
+
+    if (source.y + i === target.y && Math.abs(source.x + i) === target.x) {
+      return true
+    }
+
+    if (concerned !== 0 || concerned === "undefined") {
+      break
+    }
+  }
+
+  return false
+}
 
 export default (source, target, board) => {
   if (source.x === target.x || source.y === target.y) {
     return false
   }
 
+  //Check upwards and downwards
+
   for (let direction = -1; direction <= 2; direction += 2) {
     let threshold = -1
-    for (
-      let i = direction;
-      source.y + i !== threshold && source.x - i >= 0;
-      i += direction
-    ) {
-      const concerned = board?.[source.y + i]?.[Math.abs(source.x - i)]
+    const payload = { source, target, direction, board, threshold }
 
-      if (source.y + i === target.y && Math.abs(source.x - i) === target.x) {
-        return true
-      }
-
-      if (concerned !== 0 || concerned === "undefined") {
-        break
-      }
+    if (checkLeft(payload) || checkRight(payload)) {
+      return true
     }
 
-    for (
-      let i = direction;
-      source.y + i !== threshold && source.x + i <= 7;
-      i += direction
-    ) {
-      const concerned = board?.[source.y + i]?.[Math.abs(source.x + i)]
-
-      if (source.y + i === target.y && Math.abs(source.x + i) === target.x) {
-        return true
-      }
-
-      if (concerned !== 0 || concerned === "undefined") {
-        break
-      }
-    }
     threshold = 8
   }
 
